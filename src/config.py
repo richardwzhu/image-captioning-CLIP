c16946ce185f7c90cb406ad01d1295193213c232
Richard Zhu <richardzhu78@gmail.com>
2023-04-22 12:46:46 -0400

    Add full CLIP model and functions for training (#2)
    Co-authored-by: Richard Zhu <richardzhu@richards-mbp.mynetworksettings.com>

--- src/config.py
+++ src/config.py
@@ -8,9 +8,34 @@ default_config = {
     "train_size": 0.8,
     "batch_size": 32,
     "num_workers": 4,
+    "image_encoder_lr": 1e-4,
+    "text_encoder_lr": 1e-5,
+    "projection_head_lr": 1e-3,
+    "weight_decay": 1e-3,
+    "patience": 1,
+    "factor": 0.8,
+    "epochs": 2,
+    "device": "cpu",
 
+    # Image normalization parameters
+    "image_size": 224,
+
+    # Tokenizer parameters
     "text_tokenizer": "distilbert-base-uncased",
     "max_length": 200,
 
-    "image_size": 224,
+    # Econder parameters
+    "image_encoder": "resnet50",
+    "text_encoder": "distilbert-base-uncased",
+    "pretrained": True,
+    "trainable": True,
+
+    # Projection head parameters
+    "image_embedding": 2048,
+    "text_embedding": 768,
+    "projection_dim": 256,
+    "dropout": 0.1,
+
+    # CLIP parameters
+    "temperature": 1
 }
---
+++ src/train_eval_utils.py
@@ -0,0 +1,52 @@
+from tqdm.autonotebook import tqdm
+
+
+def train_epoch(model, train_loader, optimizer, lr_scheduler, step, device):
+    loss_meter = AvgMeter()
+    tqdm_object = tqdm(train_loader, total=len(train_loader))
+    for batch in tqdm_object:
+        batch = {k: v.to(device) for k, v in batch.items() if k != "caption"}
+        optimizer.zero_grad()
+        loss = model(batch)
+        loss.backward()
+        optimizer.step()
+        if step == "batch":
+            lr_scheduler.step()
+
+        count = batch["image"].size(0)
+        loss_meter.update(loss.item(), count)
+
+        tqdm_object.set_postfix(train_loss=loss_meter.avg)
+    return loss_meter
+
+
+def valid_epoch(model, valid_loader, device):
+    loss_meter = AvgMeter()
+    tqdm_object = tqdm(valid_loader, total=len(valid_loader))
+    for batch in tqdm_object:
+        batch = {k: v.to(device) for k, v in batch.items() if k != "caption"}
+        loss = model(batch)
+
+        count = batch["image"].size(0)
+        loss_meter.update(loss.item(), count)
+
+        tqdm_object.set_postfix(valid_loss=loss_meter.avg)
+    return loss_meter
+
+
+class AvgMeter:
+    def __init__(self, name="Metric"):
+        self.name = name
+        self.reset()
+
+    def reset(self):
+        self.avg, self.sum, self.count = [0] * 3
+
+    def update(self, val, count=1):
+        self.count += count
+        self.sum += val * count
+        self.avg = self.sum / self.count
+
+    def __repr__(self):
+        text = f"{self.name}: {self.avg:.4f}"
+        return text
