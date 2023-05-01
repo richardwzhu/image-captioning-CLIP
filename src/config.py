352c4d6aac0be53768238a9d34b8f268fe74aeee
Leo Ding <51973536+leoldding@users.noreply.github.com>
2023-05-01 19:50:45 -0400

    Update resnet50 model and add experiment 1 (#9)
    * Add resnet50 model
    
    * added self-trained resnet50 model
    
    * Update resnet50 parameter and add experiment 1

--- src/config.py
+++ src/config.py
@@ -38,6 +38,46 @@ default_config = {
     "temperature": 1
 }
 
+config_1 = {
+    "raw_file_path": "../input/raw/flickr30k/results.csv",
+    "clean_file_path": "../input/clean/flickr30k/captions.csv",
+    "train_image_path": "../input/raw/flickr30k/Images",
+    "test_image_path": "../input/raw/flickr30k/Images",
+    "train_size": 0.8,
+    "batch_size": 32,
+    "num_workers": 4,
+    "image_encoder_lr": 1e-4,
+    "text_encoder_lr": 1e-5,
+    "projection_head_lr": 1e-3,
+    "weight_decay": 1e-3,
+    "patience": 1,
+    "factor": 0.8,
+    "epochs": 2,
+    "device": "cuda:0",
+
+    # Image normalization parameters
+    "image_size": 224,
+
+    # Tokenizer parameters
+    "text_tokenizer": "distilbert-base-uncased",
+    "max_length": 200,
+
+    # Encoder parameters
+    "image_encoder": "resnet50",
+    "text_encoder": "distilbert-base-uncased",
+    "pretrained": True,
+    "trainable": True,
+
+    # Projection head parameters
+    "image_embedding": 2048,
+    "text_embedding": 768,
+    "projection_dim": 256,
+    "dropout": 0.1,
+
+    # CLIP parameters
+    "temperature": 1
+}
+
 config_2 = {
     "raw_file_path": "../input/raw/flickr30k/results.csv",
     "clean_file_path": "../input/clean/flickr30k/captions.csv",
